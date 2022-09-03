6d3a257d89e801d1d246ca07d8543bd53d89cd13
Bannerets <comonoid@protonmail.com>
2022-09-03 16:09:45 +0000

    Add a using-prebuilt-tdlib.js example

---
+++ examples/using-prebuilt-tdlib.js
@@ -0,0 +1,21 @@
+const { Client } = require('tdl')
+const { TDLib } = require('tdl-tdlib-addon')
+const { getTdjson } = require('prebuilt-tdlib')
+
+const tdlib = new TDLib(getTdjson())
+const client = new Client(tdlib, {
+  apiId: 2222, // Your api_id
+  apiHash: 'YOUR_API_HASH'
+})
+
+client.on('error', console.error)
+
+async function main() {
+  await client.connectAndLogin()
+
+  console.log(await client.invoke({ _: 'getMe' }))
+
+  await client.close()
+}
+
+main().catch(console.error)
