94ad1685fcc2727f02200c4811c7542d4db3606d
Bannerets <comonoid@protonmail.com>
2023-06-21 19:18:16 +0000

    Update dev dependencies
    Updates babel and @types/node. This keeps eslint at @7 for now
    
    @babel/env now targets node 12.0.0

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,12 @@ module.exports = {
   'presets': [
     ['@babel/env', {
       'targets': {
-        'node': '10.0.0'
+        'node': '12.0.0'
       }
     }],
     '@babel/preset-flow'
   ],
   'plugins': [
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties'
   ]
 }
