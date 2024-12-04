3aee94f4d549f0cc44c577a20ff29a0395744f22
eilvelia <hi@eilvelia.cat>
2024-12-04 16:02:02 +0000

    update scripts/run-prebuilt-tdlib.sh

--- scripts/run-prebuilt-tdlib.sh
+++ scripts/run-prebuilt-tdlib.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
-
 # Requires gh, jq
+
+set -euo pipefail
+
+if [ $# -lt 1 ]; then
+  echo "Too few arguments: expected TDLib version"
+  exit 1
+fi
 
 version=$1
 npm_patch="${2:-0}"
 
-if [ -z "$version" ]; then
-  echo "Not enough arguments: expected TDLib version"
-  exit 1
+if [ "$version" = "latest" ]; then
+  commit=$(gh api repos/tdlib/td/commits)
+else
+  commit=$(gh search commits "Update version to $version" -R tdlib/td --json commit,sha)
 fi
 
-commit=$(gh search commits "Update version to $version" -R tdlib/td --json sha --jq '.[0].sha')
-
-if [ -z "$commit" ]; then
+if [ "$commit" = "[]" ]; then
   echo "Cannot find version $version"
   exit 1
 fi
 
-command="gh workflow run prebuilt-tdlib.yml --ref develop -f tdlib=$commit -f npm-patch=$npm_patch"
+message=$(echo $commit | jq -r '.[0].commit.message')
+sha=$(echo $commit | jq -r '.[0].sha')
 
-echo "TDLib commit: https://github.com/tdlib/td/commit/$commit"
+command="gh workflow run prebuilt-tdlib.yml --ref develop -f tdlib=$sha -f npm-patch=$npm_patch"
+
+echo "TDLib commit: https://github.com/tdlib/td/commit/$sha"
+echo "Message: \"$message\""
 echo "Command: $command"
 read -p "Run? (Ctrl-c or n to exit) " answer
 case $answer in
